bcd9f203784584512ce2034d18a37dc75f94aa7f
jasonteunissen <jasonteunissen@gmail.com>
2019-09-08 17:53:41 +0200

    homepage
    homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,24 +4,77 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import Icon from '../components/Icon'
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <ul>
-      <li>
-        <Link to="/massages">Massages</Link>
-      </li>
-      <li>
-        <Link to="/skincare">Skin</Link>
-      </li>
-      <li>
-        <Link to="/wax">Wax</Link>
-      </li>
-    </ul>
+    <SEO />
+
+    <header className="header header-homepage">
+      <div className="headercontent">
+        <h3>
+          Holistic Kiwi - Skin & Massage
+        </h3>
+        <h1>
+          Rediscover your confidence
+        </h1>
+        <button
+          className="btn btn-primary"
+          href="https://reactjs.org"
+          type="button"
+        >
+        Book Now
+        </button>
+      </div>
+    </header>
+
+
+
+    <main className="main-homepage">
+        <h2>
+          What are you looking for?
+        </h2>
+        <div className="card-container card-container-homepage">
+          <Link to="/massages">
+            <div className="card">
+              <Icon name={'spa-lotus'} color="primary" className="icn" alt="logo" />
+              <h6>
+              Massages
+              </h6>
+            </div>
+          </Link>
+
+          <Link to="/skincare">
+            <div className="card">
+              <Icon name={'spa-lotus'} color="primary" className="icn" alt="logo" />
+              <h6>
+              Skin Treatments
+              </h6>
+            </div>
+          </Link>
+
+          <Link to="/wax">
+            <div className="card">
+              <Icon name={'spa-lotus'} color="primary" className="icn" alt="logo" />
+              <h6>
+              Waxing
+              </h6>
+            </div>
+          </Link>
+
+          <Link to="/wax">
+            <div className="card">
+              <Icon name={'spa-lotus'} color="primary" className="icn" alt="logo" />
+              <h6>
+              I need advice
+              </h6>
+            </div>
+          </Link>
+        </div>
+
+
+
+      </main>
   </Layout>
 )
 
