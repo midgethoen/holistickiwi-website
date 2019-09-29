3cda362ae21e01e64da730b4235e5834f6858cec
jasonteunissen <jasonteunissen@gmail.com>
2019-09-29 19:18:15 +0200

    footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -13,8 +13,17 @@ export default () => (
         <Link to="/view-results">View Results</Link>
         <Link to="/organic-skincare-consultancy">Organic Skincare Consultancy</Link>
       </div>
-      <div className="col"></div>
-      <div className="col"></div>
+      <div className="col">
+        <Link to="/about-us">Relaxation Massage</Link>
+        <Link to="/about-us">Therapeutic Massage</Link>
+
+        <Link to="/about-us">Basic Facials</Link>
+      </div>
+      <div className="col">
+        <a href='#'>Facebook</a>
+        <a href='#'>Instagram</a>
+        <a href='#'>Linkedin</a>
+      </div>
       <div className="col">
         <p>
           Kruisstraat 39-41 2611 ML Delft The Netherlands 06 192 833 21
