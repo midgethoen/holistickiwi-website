fe8cc6be26f4e41d9321632219c9f552b3d8eb68
jasonteunissen <jasonteunissen@gmail.com>
2019-09-29 18:07:32 +0200

    more css refactoring

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,7 +27,7 @@ const IndexPage = () => (
       <h2>What are you looking for?</h2>
       <div className="card-container card-container-homepage">
         <Link to="/massages">
-          <div className="card">
+
             <Icon
               name={"spa-lotus"}
               color="primary"
@@ -35,11 +35,11 @@ const IndexPage = () => (
               alt="logo"
             />
             <h6>Massages</h6>
-          </div>
+
         </Link>
 
         <Link to="/skincare">
-          <div className="card">
+
             <Icon
               name={"spa-lotus"}
               color="primary"
@@ -47,11 +47,11 @@ const IndexPage = () => (
               alt="logo"
             />
             <h6>Skin Treatments</h6>
-          </div>
+
         </Link>
 
         <Link to="/wax">
-          <div className="card">
+
             <Icon
               name={"spa-lotus"}
               color="primary"
@@ -59,11 +59,11 @@ const IndexPage = () => (
               alt="logo"
             />
             <h6>Waxing</h6>
-          </div>
+
         </Link>
 
         <Link to="/wax">
-          <div className="card">
+
             <Icon
               name={"spa-lotus"}
               color="primary"
@@ -71,7 +71,7 @@ const IndexPage = () => (
               alt="logo"
             />
             <h6>I need advice</h6>
-          </div>
+
         </Link>
       </div>
     </main>
@@ -81,32 +81,31 @@ const IndexPage = () => (
       <h5>Most Popular</h5>
       <div className="card-container card-container-treatments">
       <Link to="/massages">
-        <div className="card">
-          <h3>Therapeutic Massages</h3>
-          <h3>90 m</h3>
-          <span>Improves poor circulation</span>
-          <span>Injuries (Office/Sport, RSI, Neck shoulder)</span>
-          <span>Muscle Tension (Severe)</span>
-          <span>MuscleTension (Mild)</span>
-          <span>Lymph swelling/Odema</span>
-          <a className="btn btn-primary">Book Now</a>
-        </div>
+
+        <h3>Therapeutic Massages</h3>
+        <h3>90 m</h3>
+        <span>Improves poor circulation</span>
+        <span>Injuries (Office/Sport, RSI, Neck shoulder)</span>
+        <span>Muscle Tension (Severe)</span>
+        <span>MuscleTension (Mild)</span>
+        <span>Lymph swelling/Odema</span>
+        <a className="btn btn-primary">Book Now</a>
+
       </Link>
-      <Link to="/massages">
-        <div className="card">
-          <h6 className="card-headertag">most populair</h6>
-          <h3>Therapeutic Massages</h3>
-          <h3>90 m</h3>
-          <span>Improves poor circulation</span>
-          <span>Injuries (Office/Sport, RSI, Neck shoulder)</span>
-          <span>Muscle Tension (Severe)</span>
-          <span>MuscleTension (Mild)</span>
-          <span>Lymph swelling/Odema</span>
-          <span>Burnout Symptoms</span>
-          <span>Signs of stress</span>
-          <span>Struggle to relax</span>
-          <a className="btn btn-primary">Book Now</a>
-        </div>
+      <Link className="mostpopular" to="/massages">
+
+        <h3>Therapeutic Massages</h3>
+        <h3>90 m</h3>
+        <span>Improves poor circulation</span>
+        <span>Injuries (Office/Sport, RSI, Neck shoulder)</span>
+        <span>Muscle Tension (Severe)</span>
+        <span>MuscleTension (Mild)</span>
+        <span>Lymph swelling/Odema</span>
+        <span>Burnout Symptoms</span>
+        <span>Signs of stress</span>
+        <span>Struggle to relax</span>
+        <a className="btn btn-primary">Book Now</a>
+
       </Link>
       </div>
     </div>
