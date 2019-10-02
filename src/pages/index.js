479a908e9bb2fcfdead56edb9f3f8460ea6a34e3
jasonteunissen <jasonteunissen@gmail.com>
2019-10-02 18:38:35 +0200

    homepage edits

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,6 +23,13 @@ const IndexPage = () => (
       </div>
     </header>
 
+    <div className="clients">
+      <p>client logo</p>
+      <p>client logo</p>
+      <p>client logo</p>
+      <p>client logo</p>
+    </div>
+
     <main className="main-homepage section-purple section-center">
       <h2>What are you looking for?</h2>
       <div className="card-container card-container-homepage">
@@ -75,6 +82,37 @@ const IndexPage = () => (
         </Link>
       </div>
     </main>
+
+    <div className="certifications">
+      <p>international massage certification 1</p>
+      <p>international massage certification 2</p>
+    </div>
+
+    <div className="section-lightpurple section-center">
+      <h2>Holistic Kiwi</h2>
+      <p>
+        Welcome to your home for Sensitive Skin Treatments, and Massage Therapy in the centre of Delft.
+      </p>
+      <p>
+        Holistic = Systems/People and their properties are viewed as wholes, not just as a collection of parts.
+      </p>
+      <p>
+        Kiwi = Kiwiana, anything or anyone originating from New Zealand.
+      </p>
+        Why Holistic Kiwi?
+
+        ✓     Natural and effective treatments for sensitive and ethnic skins.
+        ✓     Advice that compliments your treatment plan.
+        ✓     100% natural, New Zealand products that embrace science.
+        ✓     Massages catered to you individually.
+        ✓     Qualified advice for posture, work related injuries and ongoing pain/stress issues.
+        ✓     Internationally qualified therapists.
+
+      <p>
+      </p>
+      <p>
+      </p>
+    </div>
   </Layout>
 )
 
