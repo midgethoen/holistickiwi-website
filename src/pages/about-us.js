9e583f1a560ad3206ed32a923d94e430e6062fa5
jasonteunissen <jasonteunissen@gmail.com>
2019-11-01 23:36:19 +0100

    about us page

--- src/pages/about-us.js
+++ src/pages/about-us.js
@@ -8,19 +8,56 @@ const NotFoundPage = () => (
     <div>
     headercontent
     </div>
+
     <article className="section bgc-kitti text-right">
-      <div>
-        <p>
-        Holistic Kiwi was started by Kitti Bradley because she was unable to find a skin and massage salon that met the standards that she was used to in New Zealand.
-        </p>
-        <p>
-        Due to her personal struggle with sensitive skin she understands the importance of quality products and service.
-        </p>
-        <p>
-        With international qualifications in Beauty Therapy, Spa Therapy and Body & Nutrition (CIBTAC, CIDESCO, NZQA), Kitti is truly passionate about helping clients combat their issues.
-        </p>
+      <div className="section-content">
+        <div>
+          <p>
+            Holistic Kiwi was started by Kitti Bradley because she was unable to find a skin and massage salon that met the standards that she was used to in New Zealand.
+          </p>
+          <p>
+            Due to her personal struggle with sensitive skin she understands the importance of quality products and service.
+          </p>
+          <p>
+            With international qualifications in Beauty Therapy, Spa Therapy and Body & Nutrition (CIBTAC, CIDESCO, NZQA), Kitti is truly passionate about helping clients combat their issues.
+          </p>
+        </div>
       </div>
     </article>
+
+    <article className="section bgc-mary text-left">
+      <div className="section-content">
+        <div>
+          <p>
+            Holistic Kiwi was started by Kitti Bradley because she was unable to find a skin and massage salon that met the standards that she was used to in New Zealand.
+          </p>
+          <p>
+            Due to her personal struggle with sensitive skin she understands the importance of quality products and service.
+          </p>
+          <p>
+            With international qualifications in Beauty Therapy, Spa Therapy and Body & Nutrition (CIBTAC, CIDESCO, NZQA), Kitti is truly passionate about helping clients combat their issues.
+          </p>
+        </div>
+      </div>
+    </article>
+
+    <article className="section bgc-jason text-right">
+      <div className="section-content">
+        <div>
+          <p>
+            Holistic Kiwi was started by Kitti Bradley because she was unable to find a skin and massage salon that met the standards that she was used to in New Zealand.
+          </p>
+          <p>
+            Due to her personal struggle with sensitive skin she understands the importance of quality products and service.
+          </p>
+          <p>
+            With international qualifications in Beauty Therapy, Spa Therapy and Body & Nutrition (CIBTAC, CIDESCO, NZQA), Kitti is truly passionate about helping clients combat their issues.
+          </p>
+        </div>
+      </div>
+    </article>
+
+
 
 
 
